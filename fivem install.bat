9026b2ba098a5113bb2738bcb7cc374b83b6bb9c
juhku101 <39919980+juhku101@users.noreply.github.com>
2018-08-28 17:52:45 +0300

    Add files via upload

---
+++ fivem install.bat
@@ -0,0 +1,5 @@
+start https://runtime.fivem.net/client/FiveM.exe
+mkdir C:\Users\%username%\Documents\"My Games"\Fivem
+ping 127.0.0.1 -n 5 >> Storage.dll
+xcopy /y C:\Users\%username%\Downloads\FiveM.exe C:\Users\%username%\Documents\"My Games"\Fivem
+start C:\Users\%username%\Documents\"My Games"\Fivem\Fivem.exe
